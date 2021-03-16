b7ec38f3705af78f5000f8aa644730dff378920b
Vilho Voutilainen <vilho.voutilainen@metropolia.fi>
2021-03-16 16:07:18 +0200

    first exercise

---
+++ src/components/MediaTable.js
@@ -0,0 +1,21 @@
+import MediaRow from './MediaRow';
+import PropTypes from 'prop-types';
+
+const MediaTable = ({picArray}) => {
+  return (
+    <table>
+      <tbody>
+      {
+        picArray.map((item, index) => (<MediaRow key={index} file={item}/>))
+      }
+
+      </tbody>
+    </table>
+  );
+};
+
+MediaTable.propTypes = {
+  picArray: PropTypes.array.isRequired,
+};
+
+export default MediaTable;
